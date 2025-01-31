4b77a2a81785cb087f087216c82485c3470d7bf3
Jan K. S. <developer@janks.software>
2025-01-31 16:36:53 -0300

    Add SSD1306 (OLED screen) example

---
+++ examples/Barcode_OLED_small/Barcode_OLED_small.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <BarcodeGFX.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SDA_PIN 21
+#define SCL_PIN 22
+#define RESET_PIN -1
+#define I2C_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, RESET_PIN);
+
+BarcodeGFX barcode(display);
+
+void setup() {
+  Serial.begin(9600);
+
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, I2C_ADDRESS)) { 
+    Serial.println("SSD1306 allocation failed");
+    while (true) {} // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+
+  barcode.setShowDigits(false).setScale(2);
+
+  bool success = barcode.draw("01234565", 0, 0, 64);
+  if (!success) {
+    Serial.println("Invalid barcode!");
+  }
+  else {
+    Serial.println("Barcode successfully drawn!");
+  }
+
+  display.display();
+}
+
+void loop() {
+}
